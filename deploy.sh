e57b044bba7277ab3377b5afeb595b8b4cc13676
shibir07 <50862522+shibir07@users.noreply.github.com>
2019-07-15 14:50:25 +0530

    var

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "hello world" //print to screen
+# Usage: Hello World Bash Shell Script Using Variables
+# Author: Vivek Gite
+# -------------------------------------------------
+ 
+# Define bash shell variable called var 
+# Avoid spaces around the assignment operator (=)
+var="Hello World"
+ 
+# print it 
+echo "$var"
+ 
+# Another way of printing it
+printf "%s\n" "$var
