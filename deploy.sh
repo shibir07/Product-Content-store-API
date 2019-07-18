c2d484d4e3b0f3330ddbe1e1c33cbb3959ada434
shibir07 <50862522+shibir07@users.noreply.github.com>
2019-07-18 17:33:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 git clone https://github.com/shibir07/Product-Content-store-API.git
 cd Product-Content-store-API
 scp file.txt travistest@54.185.219.247:/tmp
+-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
