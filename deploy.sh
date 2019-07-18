bb56ed8c880da22ba3937ab553138a6e46f64a23
shibir07 <50862522+shibir07@users.noreply.github.com>
2019-07-18 17:36:10 +0530

    test1

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git clone https://github.com/shibir07/Product-Content-store-API.git
 cd Product-Content-store-API
-scp file.txt travistest@54.185.219.247:/tmp
--o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null file.txt travistest@54.185.219.247:/tmp
